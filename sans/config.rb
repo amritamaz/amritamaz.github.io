573aa3e539836d0755c88ad572eb4b27918e34e6
Amrita Maz <amritamaz@gmail.com>
2012-03-18 12:44:57 -0400

    hey

---
+++ sans/config.rb
@@ -0,0 +1,22 @@
+# Require any additional compass plugins here.
+
+http_path = "/"
+css_dir = "lib/css"
+sass_dir = "lib/sass"
+images_dir = "lib/img"
+javascripts_dir = "lib/js"
+
+output_style = :compact
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+preferred_syntax = :scss

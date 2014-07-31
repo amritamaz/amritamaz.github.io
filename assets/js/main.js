09f80469c2916b8c53ccfdeaa4f0e987571618a3
Amrita Mazumdar <amritamaz@gmail.com>
2014-07-31 17:33:12 -0400

    debugging highlighting links

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,16 @@
+var getLocation = function(href) {
+    var l = document.createElement("a");
+    l.href = href;
+    return l;
+};
+
 $(function() {
 var listItems = $("#menu li");
 listItems.each(function(idx, li) {
-    if ($("a",$(li)).attr('href')==window.location.pathname){
-    	$(li).addClass("active");
+    if (getLocation($("a",$(li)).attr('href')).pathname==window.location.pathname){
+    	if (getLocation($("a",$(li)).attr('href')).origin==window.location.origin){
+    		$(li).addClass("active");
+    	}
     }
 });
 
